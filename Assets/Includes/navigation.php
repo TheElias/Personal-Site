9dc39b9524c1b37f02704aaca1a454ed42ea9648
TheElias <eliasbroniecki@hotmail.com>
2021-06-10 23:30:48 -0400

    first commit

---
+++ Assets/Includes/navigation.php
@@ -0,0 +1,16 @@
+<nav class = "navbar">
+    <div class = "container">
+        <div class = "navbar-home">
+            <a href="../Index.php">
+        </div>
+        <div class = "nav>">
+            <ul>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact Me</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Elias Broniecki's Personal Site</title>
+        <link rel="stylesheet" type="text/css" href="Assets/CSS/styles.css">
+    </head>
+    
+
+    <body>
+    
+        <!--Header-->
+        <?php 
+            include("Assets/Includes/homePageHeader.php");
+        ?>
+        <!--Navigation-->
+        <?php 
+            include("Assets/Includes/navigation.php");
+        ?>
+
+
+        <div class="AboutMeSection">
+            <h1>About Me</h1>
+            <hr size = "1" width = "30%">  
+        </div>
+
+
+        <!--Footer-->
+        <?php 
+            include("Assets/Includes/header.php");
+        ?>
+    </body>
+</html>

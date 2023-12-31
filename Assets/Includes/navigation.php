6a5b2ce5235fd5618e15c19e98e04eccede58c54
TheElias <eliasbroniecki@hotmail.com>
2023-12-30 19:37:50 -0500

    Incomplete Stopping Point For Files

--- Assets/Includes/navigation.php
+++ Assets/Includes/navigation.php
@@ -1,6 +1,5 @@
 <nav class = "navbar">
     <ul>
-        <li><a href="../FartKnocker.php">Test Insert</a></li>
         <li><a href="../manager">Manager</a></li>
         <li><a href="../blog">Blog</a></li>
     </ul>
